40f146b2e8791175e56951571aff0a674c2de5cd
Aiden Cullo <aidencullo@gmail.com>
2025-10-03 14:18:25 -0400

    rename operator.py as there is a name clash w stdlib

--- all_same.py
+++ all_same.py
@@ -1,11 +1,11 @@
-
-
 def all_same(lst):
     return all(x == lst[0] for x in lst) if lst else True
+
 
 # O(n) time and space
 def all_same_operator(lst):
     return len(set(lst)) < 2
+
 
 def test():
     def tens():
@@ -13,7 +13,7 @@ def test():
 
     def one_to_ten():
         return list(range(1, 10))
-    
+
     def test_all_same():
         assert all_same(tens())
         assert not all_same(one_to_ten())
@@ -21,9 +21,10 @@ def test():
     def test_all_same_operator():
         assert all_same_operator(tens())
         assert not all_same_operator(one_to_ten())
-    
+
     test_all_same()
     test_all_same_operator()
     print("tests passed!")
+
 
 test()
