9879dc82335c176e7eadd76c0ab36bc690963efd
Aiden Cullo <aidencullo@gmail.com>
2025-10-03 12:43:53 -0400

    enhance tests

--- reduce.py
+++ reduce.py
@@ -1,6 +1,11 @@
 from functools import reduce
 
 
+def log(msg, indent=4):
+    print(" " * indent + msg)
+
+
+
 def concat_with_reduce(lst):
     def concat(x, y):
         return x + y
@@ -9,10 +14,31 @@ def concat_with_reduce(lst):
 
 
 def test_concat_with_reduce():
-    s = "hello world"
-    lst = list(s)
-    assert concat_with_reduce(lst) == s
-    print("concat tests passed!")
+    def test_blank():
+        empty_str = ""
+        no_chars = list(empty_str)
+        input = no_chars
+        expected = empty_str
+
+        result = concat_with_reduce(input)
+
+        assert result == expected
+        log("blank test passed!", 8)
+
+    def test_hello_world():
+        message = "hello world"
+        chars = list(message)
+        input = chars
+        expected = message
+
+        result = concat_with_reduce(input)
+
+        assert result == expected
+        log("hello world test passed!", 8)
+        
+    test_blank()
+    test_hello_world()
+    log("concat tests passed!")
 
 
 def flatten_with_reduce(lst):
@@ -20,14 +46,23 @@ def flatten_with_reduce(lst):
         return x + y
 
     return reduce(concat, lst, [])
-    
 
 
 def test_flatten_with_reduce():
-    nested = [[0] * 2] * 2
-    expected = [0] * 4
-    assert flatten_with_reduce(nested) == expected
-    print("flatten tests passed!")
+    def test_2_by_2():
+        nested = [[0] * 2] * 2
+        flat = [0] * 4
+        input = nested
+        expected = flat
+
+        result = flatten_with_reduce(input)
+
+        assert result == expected
+        log("2x2 tests passed!", 8)
+
+    test_2_by_2()
+    log("flatten tests passed!")
+
 
 def product_with_reduce():
     pass
