a6fef77f6619777e95a4592ce4d0ab53db595aea
Aiden Cullo <aidencullo@gmail.com>
2025-10-03 11:36:55 -0400

    concatenate with reduce

--- reduce.py
+++ reduce.py
@@ -1,5 +1,16 @@
-def concat_with_reduce():
-    pass
+from functools import reduce
+
+def concat_with_reduce(lst):
+    concat = lambda x, y: x + y
+    return reduce(concat, lst, "")
+
+def test_concat_with_reduce():
+    s = "hello world"
+    lst = list(s)
+    assert concat_with_reduce(lst) == s
+    print("concat tests passed!")
+
+
 
 def flatten_with_reduce():
     pass
@@ -13,3 +24,7 @@ def max_with_reduce():
 def min_with_reduce():
     pass
 
+def main():
+    test_concat_with_reduce()
+
+main()
