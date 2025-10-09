0b8fb1d9ea955235d3fd19c4521b5a334ce15f0a
Aiden Cullo <aidencullo@gmail.com>
2025-10-09 10:23:03 -0400

    functools reduce tests pass

--- implement_reduce.py
+++ implement_reduce.py
@@ -1,5 +1,6 @@
 from operator import mul
 from math import prod
+import functools
 
 def reduce(op, sequence):
     return sum(sequence)
@@ -7,11 +8,11 @@ def reduce(op, sequence):
 def test_functools_reduce():
     one_to_ten = list(range(1, 11))
     from operator import add
-    assert reduce(add, one_to_ten) == sum(one_to_ten)
-    assert reduce(mul, one_to_ten) == prod(one_to_ten)
-    assert reduce(max, one_to_ten) == max(one_to_ten)
-    assert reduce(min, one_to_ten) == min(one_to_ten)
-    print("all my reduce tests passed!")
+    assert functools.reduce(add, one_to_ten) == sum(one_to_ten)
+    assert functools.reduce(mul, one_to_ten) == prod(one_to_ten)
+    assert functools.reduce(max, one_to_ten) == max(one_to_ten)
+    assert functools.reduce(min, one_to_ten) == min(one_to_ten)
+    print("all my functools.reduce tests passed!")
 
 def test_my_reduce():
     one_to_ten = list(range(1, 11))
@@ -22,5 +23,5 @@ def test_my_reduce():
     assert reduce(min, one_to_ten) == min(one_to_ten)
     print("all my reduce tests passed!")
 
-test_my_reduce()
+# test_my_reduce()
 test_functools_reduce()
