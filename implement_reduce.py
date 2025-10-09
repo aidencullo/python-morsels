9ec251ae041dc0a311898a39da401abaef03c2c5
Aiden Cullo <aidencullo@gmail.com>
2025-10-09 10:52:11 -0400

    test my reduce again the functools version

--- implement_reduce.py
+++ implement_reduce.py
@@ -1,4 +1,4 @@
-from operator import mul
+from operator import mul, add
 from math import prod
 import functools
 
@@ -23,24 +23,13 @@ def test_head():
     assert head([]) is None
     print("all my head tests passed!")
 
-def test_functools_reduce():
-    one_to_ten = list(range(1, 11))
-    from operator import add
-    assert functools.reduce(add, one_to_ten) == sum(one_to_ten)
-    assert functools.reduce(mul, one_to_ten) == prod(one_to_ten)
-    assert functools.reduce(max, one_to_ten) == max(one_to_ten)
-    assert functools.reduce(min, one_to_ten) == min(one_to_ten)
-    print("all my functools.reduce tests passed!")
-
 def test_my_reduce():
     one_to_ten = list(range(1, 11))
-    from operator import add
-    assert reduce(add, one_to_ten) == sum(one_to_ten)
-    assert reduce(mul, one_to_ten) == prod(one_to_ten)
-    assert reduce(max, one_to_ten) == max(one_to_ten)
-    assert reduce(min, one_to_ten) == min(one_to_ten)
+    assert reduce(add, one_to_ten) == functools.reduce(add, one_to_ten)
+    assert reduce(mul, one_to_ten) == functools.reduce(mul, one_to_ten)
+    assert reduce(max, one_to_ten) == functools.reduce(max, one_to_ten)
+    assert reduce(min, one_to_ten) == functools.reduce(min, one_to_ten)
     print("all my reduce tests passed!")
 
 test_my_reduce()
-test_functools_reduce()
 test_head()
